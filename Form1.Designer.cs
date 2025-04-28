9e97444c28cf404079ed4d9852a3291cc819dd94
diatanuchit <anuchit.aree.a9f@ap.denso.com>
2025-04-28 16:05:05 +0700

    test1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,31 +29,417 @@
         private void InitializeComponent()
         {
             button1 = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            comboBox1 = new ComboBox();
+            label14 = new Label();
+            label15 = new Label();
+            pdi_partnumber1 = new Label();
+            label17 = new Label();
+            label18 = new Label();
+            trace_lot_no = new TextBox();
+            tag_serial_no = new TextBox();
+            pdi_partnumber = new TextBox();
+            label16 = new Label();
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            final_pump_part = new TextBox();
+            final_timet = new TextBox();
+            final_finish = new TextBox();
+            final_cylinder = new TextBox();
+            label13 = new Label();
+            label21 = new Label();
+            label19 = new Label();
+            label20 = new Label();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(63, 72);
+            button1.Location = new Point(242, 33);
             button1.Name = "button1";
             button1.Size = new Size(87, 40);
             button1.TabIndex = 0;
-            button1.Text = "button1";
+            button1.Text = "Search";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(97, 33);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(139, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(22, 119);
+            label1.Name = "label1";
+            label1.Size = new Size(99, 15);
+            label1.TabIndex = 2;
+            label1.Text = "cylinder_grinding";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(22, 162);
+            label2.Name = "label2";
+            label2.Size = new Size(106, 15);
+            label2.TabIndex = 2;
+            label2.Text = "element_matching\r\n";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(22, 197);
+            label3.Name = "label3";
+            label3.Size = new Size(156, 15);
+            label3.TabIndex = 2;
+            label3.Text = "assy_stopper_valve station_1";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(22, 226);
+            label4.Name = "label4";
+            label4.Size = new Size(156, 15);
+            label4.TabIndex = 2;
+            label4.Text = "assy_stopper_valve station_2";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(22, 257);
+            label5.Name = "label5";
+            label5.Size = new Size(141, 15);
+            label5.TabIndex = 2;
+            label5.Text = "assy_oil_seal_press_fitting";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(22, 288);
+            label6.Name = "label6";
+            label6.Size = new Size(99, 15);
+            label6.TabIndex = 2;
+            label6.Text = "assy_seal_stopper";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(22, 319);
+            label7.Name = "label7";
+            label7.Size = new Size(137, 15);
+            label7.TabIndex = 2;
+            label7.Text = "assy_load_seat_assembly";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(22, 354);
+            label8.Name = "label8";
+            label8.Size = new Size(113, 15);
+            label8.TabIndex = 2;
+            label8.Text = "assy_pcv_tightening";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(22, 388);
+            label9.Name = "label9";
+            label9.Size = new Size(143, 15);
+            label9.TabIndex = 2;
+            label9.Text = "assy_pulsation_tightening";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(22, 425);
+            label10.Name = "label10";
+            label10.Size = new Size(97, 15);
+            label10.TabIndex = 2;
+            label10.Text = "assy_air_leak_test";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(22, 496);
+            label11.Name = "label11";
+            label11.Size = new Size(249, 15);
+            label11.TabIndex = 2;
+            label11.Text = "final_inspection_match_serial_cylinder_and_fg";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(22, 456);
+            label12.Name = "label12";
+            label12.Size = new Size(65, 15);
+            label12.TabIndex = 2;
+            label12.Text = "inspection ";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(785, 141);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 3;
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(6, 16);
+            label14.Name = "label14";
+            label14.Size = new Size(80, 15);
+            label14.TabIndex = 2;
+            label14.Text = "finished good";
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Location = new Point(112, 16);
+            label15.Name = "label15";
+            label15.Size = new Size(77, 15);
+            label15.TabIndex = 2;
+            label15.Text = "PDI Tag serial";
+            // 
+            // pdi_partnumber1
+            // 
+            pdi_partnumber1.AutoSize = true;
+            pdi_partnumber1.Location = new Point(216, 16);
+            pdi_partnumber1.Name = "pdi_partnumber1";
+            pdi_partnumber1.Size = new Size(70, 15);
+            pdi_partnumber1.TabIndex = 2;
+            pdi_partnumber1.Text = "partnumber";
+            // 
+            // label17
+            // 
+            label17.AutoSize = true;
+            label17.Location = new Point(11, 33);
+            label17.Name = "label17";
+            label17.Size = new Size(80, 15);
+            label17.TabIndex = 2;
+            label17.Text = "Serial number";
+            // 
+            // label18
+            // 
+            label18.AutoSize = true;
+            label18.Location = new Point(13, 61);
+            label18.Name = "label18";
+            label18.Size = new Size(192, 45);
+            label18.TabIndex = 2;
+            label18.Text = "PDI Tag  serial no : 6017646 \r\nFinish goods trace lot no : 03J05272\r\nCylinder serial no : 03J03650  \r\n";
+            // 
+            // trace_lot_no
+            // 
+            trace_lot_no.Location = new Point(6, 34);
+            trace_lot_no.Name = "trace_lot_no";
+            trace_lot_no.Size = new Size(98, 23);
+            trace_lot_no.TabIndex = 1;
+            // 
+            // tag_serial_no
+            // 
+            tag_serial_no.Location = new Point(112, 34);
+            tag_serial_no.Name = "tag_serial_no";
+            tag_serial_no.Size = new Size(98, 23);
+            tag_serial_no.TabIndex = 1;
+            // 
+            // pdi_partnumber
+            // 
+            pdi_partnumber.Location = new Point(216, 34);
+            pdi_partnumber.Name = "pdi_partnumber";
+            pdi_partnumber.Size = new Size(154, 23);
+            pdi_partnumber.TabIndex = 1;
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.Location = new Point(11, 9);
+            label16.Name = "label16";
+            label16.Size = new Size(76, 15);
+            label16.TabIndex = 2;
+            label16.Text = "TRACE BACK";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(trace_lot_no);
+            groupBox1.Controls.Add(tag_serial_no);
+            groupBox1.Controls.Add(pdi_partnumber);
+            groupBox1.Controls.Add(label14);
+            groupBox1.Controls.Add(pdi_partnumber1);
+            groupBox1.Controls.Add(label15);
+            groupBox1.Location = new Point(13, 607);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(447, 66);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "pdi";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(final_pump_part);
+            groupBox2.Controls.Add(final_timet);
+            groupBox2.Controls.Add(final_finish);
+            groupBox2.Controls.Add(final_cylinder);
+            groupBox2.Controls.Add(label13);
+            groupBox2.Controls.Add(label21);
+            groupBox2.Controls.Add(label19);
+            groupBox2.Controls.Add(label20);
+            groupBox2.Location = new Point(19, 535);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(607, 66);
+            groupBox2.TabIndex = 4;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "final_inspection_match_serial_cylinder_and_fg";
+            // 
+            // final_pump_part
+            // 
+            final_pump_part.Location = new Point(6, 34);
+            final_pump_part.Name = "final_pump_part";
+            final_pump_part.Size = new Size(98, 23);
+            final_pump_part.TabIndex = 1;
+            // 
+            // final_timet
+            // 
+            final_timet.Location = new Point(112, 34);
+            final_timet.Name = "final_timet";
+            final_timet.Size = new Size(98, 23);
+            final_timet.TabIndex = 1;
+            // 
+            // final_finish
+            // 
+            final_finish.Location = new Point(388, 34);
+            final_finish.Name = "final_finish";
+            final_finish.Size = new Size(154, 23);
+            final_finish.TabIndex = 1;
+            // 
+            // final_cylinder
+            // 
+            final_cylinder.Location = new Point(216, 34);
+            final_cylinder.Name = "final_cylinder";
+            final_cylinder.Size = new Size(154, 23);
+            final_cylinder.TabIndex = 1;
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(6, 16);
+            label13.Name = "label13";
+            label13.Size = new Size(65, 15);
+            label13.TabIndex = 2;
+            label13.Text = "pump_part";
+            // 
+            // label21
+            // 
+            label21.AutoSize = true;
+            label21.Location = new Point(388, 16);
+            label21.Name = "label21";
+            label21.Size = new Size(85, 15);
+            label21.TabIndex = 2;
+            label21.Text = "finished goods";
+            // 
+            // label19
+            // 
+            label19.AutoSize = true;
+            label19.Location = new Point(216, 16);
+            label19.Name = "label19";
+            label19.Size = new Size(96, 15);
+            label19.TabIndex = 2;
+            label19.Text = "cylinder serial no";
+            // 
+            // label20
+            // 
+            label20.AutoSize = true;
+            label20.Location = new Point(112, 16);
+            label20.Name = "label20";
+            label20.Size = new Size(67, 15);
+            label20.TabIndex = 2;
+            label20.Text = "time stamp";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1180, 730);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(comboBox1);
+            Controls.Add(label18);
+            Controls.Add(label17);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label16);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button button1;
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private ComboBox comboBox1;
+        private Label label14;
+        private Label label15;
+        private Label pdi_partnumber1;
+        private Label label17;
+        private Label label18;
+        private TextBox trace_lot_no;
+        private TextBox tag_serial_no;
+        private TextBox pdi_partnumber;
+        private Label label16;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private TextBox final_pump_part;
+        private TextBox final_timet;
+        private TextBox final_finish;
+        private TextBox final_cylinder;
+        private Label label13;
+        private Label label21;
+        private Label label19;
+        private Label label20;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -11,10 +11,81 @@ namespace testsca
 
         private void button1_Click(object sender, EventArgs e)
         {
-            using (var db = new scaContext()) 
-            { 
-                var pdi = db.Pdis.ToList();
+            tag_serial_no.Text = "";
+            trace_lot_no.Text = "";
+            pdi_partnumber.Text = "";
+            final_pump_part.Text = "";
+            final_timet.Text = "";
+            final_cylinder.Text = "";
+            final_finish.Text = "";
+
+            var searchdata = textBox1.Text.Trim();
+            if (searchdata == null || searchdata.Length == 0) { return; }
+
+
+            if (textBox1.Text.Length == 7)
+            {
+               (string pditag,string finishgoods ) =  PDI_search(searchdata);
+
+
+            }
+            else if (textBox1.Text.Length == 8)
+            {
+                Final_search(searchdata);
+
+
+            }
+
+        }
+
+
+
+        private (string , string ) PDI_search(string searchdata)
+        {
+            try
+            {
+                using (var db = new scaContext())
+                {
+                    var tag = Convert.ToDouble(searchdata);
+                    var tagserialno = db.Pdis.Where(x => x.TagSerialNo == tag).ToList();
+                    if (tagserialno.Count > 0)
+                    {
+                        var pdidata = tagserialno.FirstOrDefault();
+
+                        tag_serial_no.Text = pdidata?.TagSerialNo.ToString();
+                        trace_lot_no.Text = pdidata?.TraceLotNo;
+                        pdi_partnumber.Text = pdidata?.PartsNo;
+
+                        return tag_serial_no.Text , pdidata?.TraceLotNo;
+                    }
+                }
+            }
+            catch
+            {
+
+                return ("", "");
+            }
+
+        }
+        private void Final_search(string searchdata)
+        {
+            using (var db = new scaContext())
+            {
+                var data = db.FinalInspectionMatchSerials.
+                    Where(x => x.DataSn1CutFromI == searchdata || x.CylinderSerialCutFromJ == searchdata).ToList();
+
+                if (data.Count > 0)
+                {
+                    var finaltable = data.FirstOrDefault();
+
+                    final_pump_part.Text = finaltable?.DensoPartNoPumpPartNumber.ToString();
+                    final_timet.Text = finaltable?.Datetime.ToString();
+                    final_cylinder.Text = finaltable?.CylinderSerialCutFromJ;
+                    final_finish.Text = finaltable?.DataSn1CutFromI;
+                }
             }
         }
+
+
     }
 }
